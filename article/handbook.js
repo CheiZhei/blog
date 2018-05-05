28d55b68aa1d1297eac66516c22fad3b8bfedfbb
CheiZhei <139113163@qq.com>
2018-05-05 22:42:53 +0800

    换文件

---
+++ article/handbook.js
@@ -0,0 +1,134 @@
+[
+  {
+    title: 'javscript手册',
+    book: [
+      {
+        title: 'Array对象',
+        toname: 'article/javascript-Array.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: 'Boolen对象',
+        toname: 'article/javascript-Boolean.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: 'Date对象',
+        toname: 'article/javascript-Date.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: 'String对象',
+        toname: '',
+        content: `## string对象
+编写中...`,
+        nav:[]
+      },
+      {
+        title: 'Number对象',
+        toname: '',
+        content: `## number对象
+编写中...`,
+        nav:[]
+      }
+    ]
+  },
+  {
+    title: 'vue2.0开发API手册',
+    book: [
+      {
+        title: '全局配置',
+        toname: 'article/vue2.0/globalConfig.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '全局IPA',
+        toname: 'article/vue2.0/globalApi.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '选项/数据',
+        toname: 'article/vue2.0/optionsOfData.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '选项/DOM',
+        toname: 'article/vue2.0/optionsOfDOM.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '选项/生命周期钩子',
+        toname: 'article/vue2.0/optionsOfHook.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '选项/资源',
+        toname: 'article/vue2.0/optionsOfResource.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '选项/组合',
+        toname: 'article/vue2.0/optionsOfGourp.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '选项/其它',
+        toname: 'article/vue2.0/optionsOfOther.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '实例属性',
+        toname: 'article/vue2.0/instanceArrtibute.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '实例方法/数据',
+        toname: 'article/vue2.0/instanceMethodOfData.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '实例方法/事件',
+        toname: 'article/vue2.0/instanceMethodOfEvent.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '实例方法/生命周期',
+        toname: 'article/vue2.0/instanceMethodOfHook.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '指令',
+        toname: 'article/vue2.0/directive.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '特殊特性',
+        toname: 'article/vue2.0/specialfeatures.md',
+        content: '',
+        nav:[]
+      },
+      {
+        title: '内置组件',
+        toname: 'article/vue2.0/buitlInComponent.md',
+        content: '',
+        nav:[]
+      }
+    ]
+  }
+]
